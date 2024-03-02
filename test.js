4631804b7ec93728dff2058d4d6b6b6cb95aa444
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-03-02 21:47:03 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+
+function logDairy() {
+  for (const item of dairy) {
+    console.log(item);
+  }
+}
+
+logDairy(); // Call the function to log all dairy items
+
